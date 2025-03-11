8f2d841cdd1824a9280283cd2c9a9eb33f49435e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-11 10:04:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
+import SignInPage from './pages/auth/sign-in';
+import { neobrutalism } from '@clerk/themes';
+import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+import SignUpPage from './pages/auth/sign-up';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    element:<App />,
+    children:[
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path:'/auth/sign-in',
+    element:<SignInPage />
+  },
+  {
+    path:'/auth/sign-up',
+    element:<SignUpPage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ baseTheme: neobrutalism }}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
