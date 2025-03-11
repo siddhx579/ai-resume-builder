7fdffe53511c695dba32a8b74f1817c807d14972
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-11 10:04:57 +0530

    auth pages

---
+++ src/pages/auth/sign-in/index.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/clerk-react';
+import React from 'react';
+
+function SignInPage() {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage;
