f1a4186b87789bc2e44ef5e0e06c1c9ece7f2994
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-12 12:43:30 +0530

    Create index.jsx

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import Header from '@/components/custom/Header';
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
