9c3ff73faccd41c35d14e60ea3a5a9084757527b
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-12 12:43:21 +0530

    Update Header.jsx

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
-import { UserButton, useUser } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/clerk-react'
 
 function Header() {
-    const { user, isSignedIn } = useUser();
     return (
         <div className='w-screen p-3 px-30 flex justify-between shadow-md'>
             <Link to={'/dashboard'}>
